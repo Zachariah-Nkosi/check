370ea0b73f013bb38e27ba6a41279d83e8416c2b
Zachariah Nkosi <princezachariah3@gmail.com>
2024-07-04 12:08:06 +0200

    justchecking
    practice

---
+++ practice/index.js
@@ -0,0 +1,18 @@
+let count = 0;
+let current = document.getElementById("counter-cv");
+let total = document.getElementById("total")
+
+function increment(){
+    count += 1;
+    current.innerText = count;
+
+}
+
+function decrement(){
+    count -= 1;
+    current.innerText = count;
+}
+
+function find(){
+
+}
